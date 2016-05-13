542ef31c32e7264582f3d084cf99bc495b2dea90
Calvin Leung Huang <cleung2010@gmail.com>
2016-05-13 13:06:18 -0400

    Working diff

--- repository/diff.go
+++ repository/diff.go
@@ -20,38 +20,56 @@ func (r *Repository) DiffStatus(ref string) ([]git.DiffDelta, error) {
 		return nil, err
 	}
 
-	tree := &git.Tree{
-		Object: *obj,
+	commit, err := obj.AsCommit()
+	if err != nil {
+		return nil, err
+	}
+
+	tree, err := commit.Tree()
+	if err != nil {
+		return nil, err
 	}
 
 	h, err := r.Head()
 	if err != nil {
 		return nil, err
 	}
+
 	obj2, err := r.Lookup(h.Target())
 	if err != nil {
 		return nil, err
 	}
 
-	tree2 := &git.Tree{
-		Object: *obj2,
-	}
-
-	diffs, err := r.DiffTreeToTree(tree, tree2, &git.DiffOptions{})
+	commit2, err := obj2.AsCommit()
 	if err != nil {
 		return nil, err
 	}
 
-	log.Debugf("(git)(trace) Diffs from func: %+v | Repo ref: %s | Diff ref: %s\n", diffs, h.Target().String(), ref)
+	tree2, err := commit2.Tree()
+	if err != nil {
+		return nil, err
+	}
+
+	do, err := git.DefaultDiffOptions()
+	if err != nil {
+		return nil, err
+	}
+
+	diffs, err := r.DiffTreeToTree(tree2, tree, &do)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debugf("(git)(trace) Diffs from func: %+v | Repo ref: %s | Diff ref: %s", diffs, h.Target().String(), ref)
 
 	stats, err := diffs.Stats()
 	if err != nil {
 		return nil, err
 	}
-	log.Debugf("(git)(trace) Diffs files changed from func: %d\n", stats.FilesChanged())
+	log.Debugf("(git)(trace) Diffs files changed from func: %d", stats.FilesChanged())
 
 	n, err := diffs.NumDeltas()
-	log.Debugf("(git)(trace) Diffs num from func: %d\n", n)
+	log.Debugf("(git)(trace) Diffs num from func: %d", n)
 
 	for i := 0; i < n; i++ {
 		diff, err := diffs.GetDelta(i)
